3c693f4d69068bd79bf549ffe0a5b72d6846d1c6
Seta <sweater@azusasoft.com>
2016-05-20 13:24:30 +0800

    使用外发账号

--- app/src/main/java/com/azusasoft/sdkdemomini/framework/BaseApplication.java
+++ app/src/main/java/com/azusasoft/sdkdemomini/framework/BaseApplication.java
@@ -10,8 +10,9 @@ import com.azusasoft.facehubcloudsdk.api.FacehubApi;
 public class BaseApplication extends Application {
     private final String APP_ID = "65737441-7070-6c69-6361-74696f6e4944";
 
-    public static String USER_ID = "73be42c0-af9d-42fc-916d-bf6588559d8f";
-    public static String AUTH_TOKEN = "6a5033e05e0339849130d5780b461839";
+    //测试账号-外发Demo
+    public static String USER_ID = "e214881f-98e2-4a85-9de6-037e724a6100";
+    public static String AUTH_TOKEN = "953dca8a34bfef614237dcda5061f839";
 
     @Override
     public void onCreate() {
